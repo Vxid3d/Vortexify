9daad8b82eb4217e7f590c10abaca36f80296e06
Vxid3d <vaishalirprasad2018@gmail.com>
2026-06-22 13:15:46 -0700

    Update Vortexify.py

--- Vortexify.py
+++ Vortexify.py
@@ -1,2 +1,8 @@
 # if you see ts, hi!
 print("coming on 7/1/26")
+# sneak peek: 
+def telly(text):
+    text = "Vortexify > " + text
+    player.execute('/tellraw @p {"rawtext":[{"text":"' + str(text) + '"}]}')
+telly("Welcome, what world shall we obliterate?")
+telly("type 'help' for a list of commands")
