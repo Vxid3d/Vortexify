86a26fcae1daaaa830b35434f9ab5399897f542b
Vxid3d <vaishalirprasad2018@gmail.com>
2026-06-22 11:05:00 -0700

    Update Vortexify.js

--- Vortexify.js
+++ Vortexify.js
@@ -1,3 +1,4 @@
 // PLEASE READ ME
 // if you are coming to use the hack client and are not going to submit a pull request to fix a bug (when it happens)
 // use the .py version, this .js version is for people who want to interact and want more web support (it has more users than the python version_
+console.log("coming on 7/1/26")
