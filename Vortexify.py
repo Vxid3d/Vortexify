980570ede89e1a3959911a8786fa65f47aafa7ea
Vxid3d <vaishalirprasad2018@gmail.com>
2026-06-23 17:47:56 -0700

    Update Vortexify.py

--- Vortexify.py
+++ Vortexify.py
@@ -1,2 +1,4 @@
+# NOTE: if you want to debug/pr, do so in the JS version, it will be easier for you.
+# If you are using this as a regular joe, you cool gng.
 # Vortexify on TOP.
 print("coming on 7/14/26")
